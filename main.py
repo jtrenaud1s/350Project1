0b05774cd6deb4393ea7da1269fcf8d63cfb8f76
Zach Hall <50154183+ZachJHall@users.noreply.github.com>
2021-09-18 20:31:14 -0500

    Convert to matrix function added (#5)
    * Added mazes to folder
    
    * Add main and createMatrix function
    
    * Added mat2 and mat3

---
+++ main.py
@@ -0,0 +1,18 @@
+def createMatrix(file):
+    file = open(file, 'r')
+    lines = file.readlines()
+
+    mat = []
+
+    for line in lines:
+        mat.append([c for c in line.split()])
+
+    return mat
+    
+
+mat1 = createMatrix('maze_1.txt')
+mat2 = createMatrix('maze_2.txt')
+mat3 = createMatrix('maze_3.txt')
+
+
+
