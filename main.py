a8ed29c8c5baa61cbd3aac18e15a6632a1333a60
zachjhall <zachjohnhall@gmail.com>
2021-09-24 13:03:42 -0500

    Create graph from matrix

--- main.py
+++ main.py
@@ -9,10 +9,51 @@ def createMatrix(file):
     
     return mat
     
+def createGraph(mat):
+    graph = {}
+    for line in range(len(mat)):
+        for char in range(len(mat[line])):
+            if (str.isalpha(mat[line][char])):
+                node  = mat[line][char]
+
+                if node not in graph:
+                    graph[node] = []
+
+
+                try:
+                    if ( str.isalpha(mat[line][char-2])):
+                        graph[node].append( (mat[line][char-2], 'L' ))
+
+                except:
+                    pass
+
+                try:
+                    if ( str.isalpha( mat[line][char+2])    ):
+                        graph[node].append( (mat[line][char+2], 'R' ))
+                except:
+                    pass
+
+                try:
+                    if ( str.isalpha( mat[line-2][char])    ):
+                        if (mat[line-1][char] != '-'):
+                            graph[node].append( (mat[line-2][char], 'U' ))
+                except:
+                    pass
+
+
+                try:
+                    if ( str.isalpha( mat[line+2][char])    ):
+                        if (mat[line+1][char] != '-'):
+                            graph[node].append( (mat[line+2][char], 'D' ))
+                except:
+                    pass
+
+    return graph
 
 mat1 = createMatrix('maze_1.txt')
 mat2 = createMatrix('maze_2.txt')
 mat3 = createMatrix('maze_3.txt')
 
-
-
+graph1 = createGraph(mat1)
+graph2 = createGraph(mat2)
+graph3 = createGraph(mat3)
