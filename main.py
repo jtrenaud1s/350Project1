dc032b8a6dc744044382c0e885152b5bc31df698
Zach Hall <50154183+ZachJHall@users.noreply.github.com>
2021-09-24 20:12:19 -0500

    Create graph from matrix (#7)
    * Create graph from matrix
    
    * create Graph refactor
    
    * Find start and end of maze (#8)
    
    * Comment added
    
    * Create graph from matrix
    
    * create Graph refactor
    
    * Comment added

--- main.py
+++ main.py
@@ -22,6 +22,48 @@ def getStartEnd(mat):
 
 
     return start, end 
+    
+def createGraph(mat):
+    graph = {}
+    for line in range(len(mat)):
+        for char in range(len(mat[line])):
+            if (str.isalpha(mat[line][char])):
+                node  = mat[line][char]
+
+                if node not in graph:
+                    graph[node] = []
+
+
+                directions = {
+                    "L": (0, -2, 0, -1),
+                    "R": (0, 2, 0, 1),
+                    "U": (-2, 0, -1, 0),
+                    "D": (2, 0, 1, 0),
+
+                    # y change, x change, y for dash, x for dash
+
+                }
+
+                for item in directions:
+                    dir = item
+                    data = directions[item]
+
+
+                    try:
+                        if ( str.isalpha(mat[line + data[0]][char + data[1]] ) ):
+                            if (dir != "U" and dir != "D"):
+                                graph[node].append(     (mat[line + data[0] ][char + data[1] ], dir, (line +data[2], char + data[3]))      )
+                            elif (dir == "U"):
+                                if (mat[line-1][char] != '-'):
+                                    graph[node].append(     (mat[line + data[0] ][char + data[1] ], dir, (line + data[2], char + data[3]))      )
+
+                            elif (dir == "D"):
+                                if (mat[line+1][char] != '-'):
+                                    graph[node].append(     (mat[line + data[0] ][char + data[1] ], dir, (line + data[2], char + data[3]))      )        
+                    except:
+                        pass
+
+    return graph
 
 mat1 = createMatrix('maze_1.txt')
 mat2 = createMatrix('maze_2.txt')
@@ -31,4 +73,7 @@ s1, e1 = getStartEnd(mat1)
 s2, e2 = getStartEnd(mat2)
 s3, e3 = getStartEnd(mat3)
 
+graph1 = createGraph(mat1)
+graph2 = createGraph(mat2)
+graph3 = createGraph(mat3)
 
